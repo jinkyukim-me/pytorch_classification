005eb8b25bf0031e03fa674dea0e85686c66a3c6
Jinkyu Kim <jinkyukim.dev@gmail.com>
2019-09-16 21:50:23 +0900

    파일 생성

---
+++ tutorial_wine.py
@@ -0,0 +1,15 @@
+# Pytorch 라이브러리 임포트
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
+# scikit-learn 라이브러리 임포트
+from sklearn.datasets import load_wine
+from sklearn.model_selection import train_test_split
+# Pandas 라이브러리 임포트
+import pandas as pd
+# 와인 데이터 읽어 들이기
+wine = load_wine()
+wine
